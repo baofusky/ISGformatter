649270a530a0cd6d09205d077910bafd0b6274ee
baofusky <96409219+baofusky@users.noreply.github.com>
2026-06-01 17:42:53 +0900

    Update ISGformatter.py

--- ISGformatter.py
+++ ISGformatter.py
@@ -921,10 +921,6 @@ with tab1:
                     continue
                 if line.lower().startswith("gateway"):
                     continue
-				if line.lower().startswith("enable"):
-                    continue
-		        if line.lower().startswith("rule"):
-                    continue
                 if line.lower().startswith("from-address"):
                     continue
                 if line.lower().startswith("destination-addresses"):
