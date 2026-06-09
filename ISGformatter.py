bcc2efa8467aea135f3466ff96afc8bf81febc95
baofusky <96409219+baofusky@users.noreply.github.com>
2026-06-09 14:13:23 +0900

    Update ISGformatter.py

--- ISGformatter.py
+++ ISGformatter.py
@@ -1082,6 +1082,8 @@ localhost(config-local-user-list-local-users)# edit user {user} password
                     continue 
                 if line.lower().startswith("edit user"):
                     continue
+                if line.lower().startswith("edit ccl"):
+                    continue
 
                 if line.lower().startswith("add"):
                     continue
